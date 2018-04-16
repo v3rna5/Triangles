34f36bfef377ae93191587f0977812ed7f9464a5
Alex Noble and Verna Santos <alex@>
2018-04-16 15:18:03 -0700

    add form validation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,11 +15,26 @@ namespace Triangles.Controllers
       [Route("/onSubmit")]
        public ActionResult OnSubmit()
        {
-            TrianglesVariable myTrianglesVariable = new TrianglesVariable();
-            myTrianglesVariable.SetSideOne(Request.Query["length"]);
-            myTrianglesVariable.SetSideTwo(Request.Query["width"]);
-            myTrianglesVariable.SetSideThree(Request.Query["height"]);
-            return View("Results", myTrianglesVariable);
+         string side1=Request.Query["length"];
+         string side2=Request.Query["width"];
+         string side3=Request.Query["height"];
+
+         int num2;
+         bool validSide1=int.TryParse(side1, out num2);
+         bool validSide2=int.TryParse(side2, out num2);
+         bool validSide3=int.TryParse(side3, out num2);
+         if (validSide1==true && validSide2==true && validSide3==true)
+         {
+              TrianglesVariable myTrianglesVariable = new TrianglesVariable();
+              myTrianglesVariable.SetSideOne(Request.Query["length"]);
+              myTrianglesVariable.SetSideTwo(Request.Query["width"]);
+              myTrianglesVariable.SetSideThree(Request.Query["height"]);
+              return View("Results", myTrianglesVariable);
+         }
+         else {
+           Console.WriteLine("SORRY THIS DOESNT WORK");
+           return View("Form");
+         }
        }
     }
 }
